b591c1b3126ff49d6d33f85062630a02afb69255
John Ensley <jensley@nexant.com>
2020-05-21 18:52:56 -0600

    attempting to commit a notebook

---
+++ notebooks/notebook-scripts/test.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% pycharm={"is_executing": false}
+1 + 2 + 3
+
+# %% [markdown]
+# ## Here is some markdown.
+#
+# Blah blah blah.
+
+# %% pycharm={"name": "#%%\n", "is_executing": false}
+print('Hello World')
+
